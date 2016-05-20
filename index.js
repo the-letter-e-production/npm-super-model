435845687bc45416faaa08da1c9e634cc7566923
Eric Uldall <ericuldall@gmail.com>
2016-05-20 16:52:55 -0700

    initial  push

---
+++ index.js
@@ -0,0 +1,67 @@
+var util = require('util');
+var SuperModel = function(data){
+    if( typeof data !== "undefined" ){
+        this.data = data;
+    }else{
+        this.data = {}; 
+    }   
+};
+
+SuperModel.prototype = {
+    mapping: {}, 
+    set: function(key, val){
+        if( this.filters.hasOwnProperty(key) ){
+            val = this.filters[key](val);
+        }   
+        this.data[key] = val;
+    },  
+    get: function(key){
+        return this.data[key] || false;
+    },  
+    import: function(data){
+        for(var key in data){
+            var prop = this.mapping[key];
+            if( prop ){
+                this.set(prop, data[key]);
+            }   
+        }   
+    },  
+    export: function(){
+        return this.data;
+    },  
+    filters: {
+    }   
+};
+
+SuperModel.clone = function(options){
+    var model = function(){
+        SuperModel.call(this);
+        for(var key in options){
+            this[key] = options[key];
+        }
+    };
+    util.inherits(model, SuperModel);
+    
+    return model;
+};
+
+module.exports = SuperModel;
+
+/**
+ * Sample Usage
+ * /
+var User = SuperModel.clone({
+    mapping: {
+        em: 'email'
+    },
+    filters: {
+        email: function(val){
+            return 'fixed:' + val;
+        }
+    }
+});
+
+var user = new User;
+    user.import({em: 'eric@restorationmedia.com'});
+    console.log(user);
+// */
